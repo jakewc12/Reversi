ef801251a8a8bd10120ab0b8fe1d7290f3f54965
Stephen Williams <52682961+stephen-williamsb@users.noreply.github.com>
2023-10-24 23:34:58 -0400

    Split the tests into MutableReversiTests.java and ReversiReadTests.java for better readability

--- ReversiGame/test/MutableReversiTests.java
+++ ReversiGame/test/MutableReversiTests.java
@@ -1,4 +1,3 @@
-import model.GameDisc.DiscColor;
 import model.MutableReversi;
 import model.MutableReversiModel;
 import org.junit.Before;
@@ -6,8 +5,8 @@ import org.junit.Test;
 import org.testng.Assert;
 
 /**
- * Test meant for the MutableReversi class. These test should test every method and thrown exception
- * inside the class.
+ * Test meant for the MutableReversi class. These test should test mutation or edit method and throw
+ * appropriate exceptions inside the class.
  */
 public class MutableReversiTests {
 
@@ -18,43 +17,10 @@ public class MutableReversiTests {
     game = new MutableReversi();
   }
 
-  /**
-   * Ensure the size of the board is not less than 1.
-   */
   @Test
-  public void startGameThrowsOnInvalidSize() {
-    Assert.assertThrows(IllegalArgumentException.class, () -> game.startGame(-1));
-    Assert.assertThrows(IllegalArgumentException.class, () -> game.startGame(0));
-  }
-
-
-  @Test
-  public void getRadiusAndGetBoardSizeWorks() {
-    game.startGame(5);
-    Assert.assertEquals(11, game.getBoardSize());
-    Assert.assertEquals(5, game.getBoardRadius());
-  }
-
-  /**
-   * Check that everything outside the center tiles ring is a grey tile. The ring around the origin
-   * tile should be not grey. The origin tile itself will be grey.
-   */
-  @Test
-  public void getDiscAtFunctionsCorrectlyOnGameStart() {
-    game.startGame(5);
-    int boardRadius = 5;
-    for (int q = -boardRadius; q < boardRadius; q++) {
-      for (int r = -boardRadius; r < boardRadius; r++) {
-        for (int s = -boardRadius; s < boardRadius; s++) {
-          if (q >= -1 && q <= 1 && r >= -1 && r <= 1 && s >= -1 && s <= 1) {
-            Assert.assertNotEquals(DiscColor.GREY, game.getDiscAt(q, r, s).getColor());
-          } else {
-            Assert.assertEquals(DiscColor.GREY, game.getDiscAt(q, r, s).getColor());
-          }
-        }
-      }
-    }
-    Assert.assertEquals(DiscColor.GREY, game.getDiscAt(0, 0, 0).getColor());
+  public void nothingInMutableFunctionsBeforeGameStart() {
+    Assert.assertThrows(IllegalStateException.class, () -> game.placeDisc(0, 0, 0));
+    Assert.assertThrows(IllegalStateException.class, () -> game.skipCurrentTurn());
   }
 
   @Test
@@ -90,20 +56,5 @@ public class MutableReversiTests {
     Assert.assertThrows(IllegalArgumentException.class, () -> game.placeDisc(0, 0, -6));
   }
 
-  @Test
-  public void nothingFunctionsBeforeGameStart() {
-    Assert.assertThrows(IllegalStateException.class, () -> game.getDiscAt(0, 0, 0));
-    Assert.assertThrows(IllegalStateException.class, () -> game.placeDisc(0, 0, 0));
-    Assert.assertThrows(IllegalStateException.class, () -> game.skipCurrentTurn());
-    Assert.assertThrows(IllegalStateException.class, () -> game.gameOver());
-    Assert.assertThrows(IllegalStateException.class, () -> game.getBoardSize());
-    Assert.assertThrows(IllegalStateException.class, () -> game.getCurrentTurn());
-    Assert.assertThrows(IllegalStateException.class, () -> game.getBoardRadius());
-  }
 
-  @Test
-  public void cannotStartGameWhenGameIsStarted() {
-    game.startGame(5);
-    Assert.assertThrows(IllegalStateException.class, () -> game.startGame(4));
-  }
 }
---
+++ ReversiGame/test/ReversiReadTests.java
@@ -0,0 +1,86 @@
+import model.GameDisc.DiscColor;
+import model.MutableReversi;
+import model.ReadOnlyReversiModel;
+import org.junit.Before;
+import org.junit.Test;
+import org.testng.Assert;
+
+/**
+ * Test meant for the ReadOnlyReversiModel class. These test should test read methods and throw
+ * appropriate exceptions inside the class.
+ */
+public class ReversiReadTests {
+
+  private ReadOnlyReversiModel game = new MutableReversi();
+
+  @Before
+  public void init() {
+    game = new MutableReversi();
+  }
+
+  /**
+   * Ensure the size of the board is not less than 1.
+   */
+  @Test
+  public void startGameThrowsOnInvalidSize() {
+    Assert.assertThrows(IllegalArgumentException.class, () -> game.startGame(-1));
+    Assert.assertThrows(IllegalArgumentException.class, () -> game.startGame(0));
+  }
+
+
+  @Test
+  public void getRadiusAndGetBoardSizeWorks() {
+    game.startGame(5);
+    Assert.assertEquals(11, game.getBoardSize());
+    Assert.assertEquals(5, game.getBoardRadius());
+  }
+
+  //Tests game start methods
+
+  /**
+   * Check that everything outside the center tiles ring is a grey tile. The ring around the origin
+   * tile should be not grey. The origin tile itself will be grey.
+   */
+  @Test
+  public void getDiscAtFunctionsCorrectlyOnGameStart() {
+    game.startGame(5);
+    int boardRadius = 5;
+    for (int q = -boardRadius; q < boardRadius; q++) {
+      for (int r = -boardRadius; r < boardRadius; r++) {
+        for (int s = -boardRadius; s < boardRadius; s++) {
+          try {
+            if (q == 0 && r == 0 && s == 0) {
+              Assert.assertEquals(DiscColor.GREY, game.getDiscAt(q, r, s).getColor());
+            } else if (q >= -1 && q <= 1 && r >= -1 && r <= 1 && s >= -1 && s <= 1) {
+              Assert.assertNotEquals(DiscColor.GREY, game.getDiscAt(q, r, s).getColor());
+            } else {
+              Assert.assertEquals(DiscColor.GREY, game.getDiscAt(q, r, s).getColor());
+            }
+          } catch (Exception e) {
+            throw new AssertionError(
+                "An error occurred when getting a disc at q: " + q + " r: " + r + "s: " + s
+                    + "\nException was: " + e);
+          }
+        }
+      }
+    }
+    Assert.assertEquals(DiscColor.GREY, game.getDiscAt(0, 0, 0).getColor());
+  }
+
+  @Test
+  public void nothingInReadFunctionsBeforeGameStart() {
+    Assert.assertThrows(IllegalStateException.class, () -> game.getDiscAt(0, 0, 0));
+    Assert.assertThrows(IllegalStateException.class, () -> game.gameOver());
+    Assert.assertThrows(IllegalStateException.class, () -> game.getBoardSize());
+    Assert.assertThrows(IllegalStateException.class, () -> game.getCurrentTurn());
+    Assert.assertThrows(IllegalStateException.class, () -> game.getBoardRadius());
+  }
+
+  @Test
+  public void cannotStartGameWhenGameIsStarted() {
+    game.startGame(5);
+    Assert.assertThrows(IllegalStateException.class, () -> game.startGame(4));
+  }
+
+
+}
