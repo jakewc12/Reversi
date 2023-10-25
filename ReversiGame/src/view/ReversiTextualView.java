6662e1dc62e036f2d35dbfe37cf91aec80d0c79a
Stephen Williams <52682961+stephen-williamsb@users.noreply.github.com>
2023-10-25 01:27:25 -0400

    implemented ReversiTextualView.java

--- ReversiGame/src/view/ReversiTextualView.java
+++ ReversiGame/src/view/ReversiTextualView.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import model.ReadOnlyReversiModel;
 
 /**
- * A simple text-based rendering of the Reversi game.
+ * A simple text-based rendering of the Reversi game. This will render Black as X and white as O.
  */
 public class ReversiTextualView implements TextualView {
 
@@ -23,7 +23,39 @@ public class ReversiTextualView implements TextualView {
 
   @Override
   public String toString() {
-    return "Not Implemented";
+
+    int boardRadius = model.getBoardRadius();
+    StringBuilder returnString = new StringBuilder();
+
+    for (int q = -boardRadius; q < boardRadius; q++) {
+      //Append the start with spaces
+      for (int i = Math.abs(q); i < boardRadius; i++) {
+        returnString.append(" ");
+      }
+      for (int r = -boardRadius; r < boardRadius; r++) {
+        for (int s = -boardRadius; s < boardRadius; s++) {
+
+          //valid tile should coordinates should equal zero and be within bounds of abs(radius)
+          if (q + r + s != 0) {
+            continue;
+          }
+          //If its black place an X, if white place an O, otherwise do underscore.
+          switch (model.getDiscAt(q, r, s).getColor()) {
+            case BLACK:
+              returnString.append("X");
+              break;
+            case WHITE:
+              returnString.append("O");
+              break;
+            default:
+              returnString.append("_");
+          }
+        }
+      }
+
+
+    }
+    return returnString.toString();
   }
 
   @Override
