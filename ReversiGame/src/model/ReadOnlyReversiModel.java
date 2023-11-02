f276ce7f27d2c4b59546524bba5c802311194eac
Jake Wu-Chen <jwcgolf12@gmail.com>
2023-11-02 16:40:42 -0400

    Added new functions checkLegalMove, checkCurrnetPlayerHasLegalMoveLeft, and checkScoreOfPlayer to fit new functionality of game.

--- ReversiGame/src/model/ReadOnlyReversiModel.java
+++ ReversiGame/src/model/ReadOnlyReversiModel.java
@@ -65,7 +65,41 @@ public interface ReadOnlyReversiModel {
 
   /**
    * Returns a board in a default initial state based on the game's size.
+   *
    * @return List of HexagonCell representing a board at default state.
    */
   List<HexagonCell> getBoard();
+
+  /**
+   * Checks if a move placed at (q,r,s) is legal and returns that.
+   *
+   * @param q the q coordinate of the location.
+   * @param r the r coordinate of the location.
+   * @param s the s coordinate of the location.
+   * @return if the given move is legal or not.
+   * @throws IllegalArgumentException if the coordinates are invalid.
+   * @throws IllegalStateException    if the game hasn't started yet.
+   */
+
+  boolean checkLegalMove(int q, int r, int s);
+
+  /**
+   * Checks if the current player has any legal moves and returns that.
+   *
+   * @return if the current player has any legal moves left.
+   * @throws IllegalStateException if game hasn't started.
+   */
+  boolean checkCurrentPlayerHasLegalMovesLeft();
+
+
+  /**
+   * Returns the score or count of tiles the given player has.
+   *
+   * @param player either player one or player two.
+   * @return the score of player.
+   * @throws IllegalStateException    if the game hasn't started yet.
+   * @throws IllegalArgumentException if player is invalid.
+   */
+  int checkScoreOfPlayer(int player);
 }
+
