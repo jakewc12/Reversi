cdd1f692d8b97480602e59ab9700eba2e98594d9
Jake Wu-Chen <jwcgolf12@gmail.com>
2023-11-02 15:28:38 -0400

    Created necessary interfaces for GUI

---
+++ ReversiGame/src/view/JReversiPanel.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+import model.ReadOnlyReversiModel;
+
+public class JReversiPanel extends JPanel {
+  private ReadOnlyReversiModel model;
+}
---
+++ ReversiGame/src/view/ReversiView.java
@@ -0,0 +1,25 @@
+package view;
+
+import controller.ReversiController;
+
+/**
+ * A view for Tic-Tac-Toe: display the game board and provide visual interface for users.
+ */
+public interface ReversiView {
+
+  /**
+   * Set up the controller to handle click events in this view.
+   * @param listener the controller
+   */
+  void addClickListener(ReversiController listener);
+
+  /**
+   * Refresh the view to reflect any changes in the game state.
+   */
+  void refresh();
+
+  /**
+   * Make the view visible to start the game session.
+   */
+  void makeVisible();
+}
