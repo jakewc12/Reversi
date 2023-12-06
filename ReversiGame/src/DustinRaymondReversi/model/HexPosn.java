1d94f08ea60641e5a0c729da1cf3fb8497fb9789
Stephen Williams <stephen.williamsb@gmail.com>
2023-12-05 21:55:26 -0500

    Added info to peer review

--- ReversiGame/src/DustinRaymondReversi/model/HexPosn.java
+++ ReversiGame/src/DustinRaymondReversi/model/HexPosn.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 /**
  * Class representation of a Hexagonal coordinate. Using Axial LogicalCoordinate representation (2
- * axes)
+ * axes).
  */
 public class HexPosn {
 
